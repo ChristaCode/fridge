23180003c42928b27ff4b761b68e0bb30c359087
cookecd1 <Christa.Cooke@jhuapl.edu>
2024-01-06 18:52:55 -0500

    webpack configured

--- webpack.config.js
+++ webpack.config.js
@@ -2,37 +2,44 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js', // The entry point of your application
+  entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, 'dist'), // Output directory
-    filename: 'bundle.js', // Output bundle file name
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html', // Path to your HTML template
-    }),
-  ],
+  mode: 'development', // Set the mode
   module: {
     rules: [
       {
-        test: /\.jsx?$/, // Regex for JavaScript and JSX files
+        test: /\.jsx?$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader', // Loader for transpiling ES6 and JSX to ES5
+          loader: 'babel-loader',
         },
       },
-      // You can add more loaders here for other file types (e.g., CSS, images)
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'], // CSS loaders
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i, // Regex to match image files
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]', // Output file naming format
+              outputPath: 'images/', // Directory to output image files
+            },
+          },
+        ],
+      },
+      // Add other loaders for different file types as needed
     ],
   },
-  resolve: {
-    extensions: ['.js', '.jsx'], // Automatically resolve these file extensions
-  },
-  devServer: {
-    static: {
-      directory: path.join(__dirname, 'dist'), // or 'public', depending on your setup
-    },
-    historyApiFallback: true, // Add this line
-    compress: true,
-    port: 9000, // Port where dev server will run
-  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+  // ... other configurations
 };
