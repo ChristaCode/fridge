654b27907a96fa2bcd9eccde2abf29a2867378f9
cookecd1 <Christa.Cooke@jhuapl.edu>
2024-01-06 22:07:25 -0500

    restructure

--- src/RecipeListComponent.js
+++ src/RecipeListComponent.js
@@ -1,85 +1,75 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import './RecipeListComponent.css';
 
-const RecipeListComponent = ({ llamaRecipes, mealDBRecipes, recipes, isLoading }) => {    
+const RecipeListComponent = ({ llamaRecipes, mealDBRecipes, flaxRecipes, gptRecipes, isLoading, doneAllLoading }) => {    
     return (
         <div className="recipe-list">
-            {llamaRecipes !== null &&
-            (<div className="recipe-card">
-                <h2 className="recipe-title">{llamaRecipes.title}</h2>
-                <h3>Ingredients:</h3>
-                <ul>
-                    {llamaRecipes.ingredients.map((ingredient, i) => (
-                        <li key={i}>{ingredient}</li>
+            {isLoading ? (
+                <div className="loading-container">
+                    <div className="loading-spinner"></div>
+                </div>
+            ) : (
+                <>
+                    {llamaRecipes && llamaRecipes.ingredients && (
+                        <div className="recipe-card">
+                            <h2 className="recipe-title">{llamaRecipes.title}</h2>
+                            <h3>Ingredients:</h3>
+                            <ul>
+                                {llamaRecipes.ingredients.map((ingredient, i) => (
+                                    <li key={i}>{ingredient}</li>
+                                ))}
+                            </ul>
+                            <h3>Instructions:</h3>
+                            <ol>
+                                {llamaRecipes.instructions.map((instruction, i) => (
+                                    <li key={i}>{instruction}</li>
+                                ))}
+                            </ol>
+                        </div>
+                    )}
+                    {mealDBRecipes && mealDBRecipes.map((recipe, index) => (
+                        <div key={index} className="recipe-card">
+                            <h2 className="recipe-title">{recipe.title}</h2>
+                            <h3>Ingredients:</h3>
+                            <ul>
+                                {recipe.ingredients.map((ingredient, i) => (
+                                    <li key={i} className={recipe.elementsNotInArray.includes(ingredient) ? 'red-text' : ''}>
+                                        {ingredient}
+                                    </li>
+                                ))}
+                            </ul>
+                            <h3>Instructions:</h3>
+                            <ol>
+                                {recipe.instructions.map((instruction, i) => (
+                                    <li key={i}>{instruction}</li>
+                                ))}
+                            </ol>
+                        </div>
                     ))}
-                </ul>
-                <h3>Instructions:</h3>
-                <ol>
-                    {llamaRecipes.instructions.map((instruction, i) => (
-                        <li key={i}>{instruction}</li>
-                    ))}
-                </ol>
-            </div>
+                    {flaxRecipes && flaxRecipes.recipes && (
+                        <div className="recipe-card">
+                            <h2 className="recipe-title">{flaxRecipes.recipes.title}</h2>
+                            <h3>Ingredients:</h3>
+                            <ul>
+                                {Object.entries(flaxRecipes.recipes.ingredients).map(([ingredient, quantity], i) => (
+                                    <li key={i}>{`${ingredient}: ${quantity}`}</li>
+                                ))}
+                            </ul>
+                            <h3>Instructions:</h3>
+                            <ol>
+                                {flaxRecipes.recipes.instructions.map((instruction, i) => (
+                                    <li key={i}>{instruction}</li>
+                                ))}
+                            </ol>
+                        </div>
+                    )}
+                    {doneAllLoading && (
+                        <div className="loading-container">
+                            <div className="loading-spinner"></div>
+                        </div>
+                    )}
+                </>
             )}
-            {mealDBRecipes.map((recipe, index) => (
-            <div key={index} className="recipe-card">
-                <h2 className="recipe-title">{recipe.title}</h2>
-                <h3>Ingredients:</h3>
-                <ul>
-                    {recipe.ingredients.map((ingredient, i) => (
-                        <li key={i} className={recipe.elementsNotInArray.includes(ingredient) ? 'red-text' : ''}>
-                            {ingredient}
-                        </li>
-                    ))}
-                </ul>
-                    <h3>Instructions:</h3>
-                    <ol>
-                        {recipe.instructions.map((instruction, i) => (
-                            <li key={i}>{instruction}</li>
-                        ))}
-                    </ol>
-                </div>
-            ))}
-            {isLoading ?
-                (<div className="loading-container">
-                    <div className="loading-spinner"></div>
-                </div>) : 
-                <div className="recipe-card">
-                    <h2 className="recipe-title">{recipes.recipes.title}</h2>
-                    <h3>Ingredients:</h3>
-                    <ul>
-                        {Object.entries(recipes.recipes.ingredients).map(([ingredient, quantity], i) => (
-                            <li key={i}>{`${ingredient}: ${quantity}`}</li>
-                        ))}
-                    </ul>
-                    <h3>Instructions:</h3>
-                    <ol>
-                        {recipes.recipes.instructions.map((instruction, i) => (
-                            <li key={i}>{instruction}</li>
-                        ))}
-                    </ol>
-                </div>
-            }
-            {/* {isLoading ?
-                (<div className="loading-container">
-                    <div className="loading-spinner"></div>
-                </div>) : recipes.map((recipe, index) => (
-                <div key={index} className="recipe-card">
-                    <h2 className="recipe-title">{recipe.title}</h2>
-                    <h3>Ingredients:</h3>
-                    <ul>
-                        {recipe.ingredients.map((ingredient, i) => (
-                            <li key={i}>{ingredient}</li>
-                        ))}
-                    </ul>
-                    <h3>Instructions:</h3>
-                    <ol>
-                        {recipe.instructions.split('\n').map((instruction, i) => (
-                            <li key={i}>{instruction}</li>
-                        ))}
-                    </ol>
-                </div>
-            ))} */}
         </div>
     );
 };
