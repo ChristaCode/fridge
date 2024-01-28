566861c5ad87cd89b40df6bf343c2cf21aa98d2b
Zoe <zlongen1@gmail.com>
2024-01-28 13:22:35 -0500

    Recipe_Wrapper_Z

--- src/App.js
+++ src/App.js
@@ -65,6 +65,7 @@ const App = () => {
 
     return (
         <div className="App">
+        <span className="Recipe Wrapper">
             <img src={logo} alt="FeastFinder Logo" style={{ maxWidth: '250px', height: 'auto' }} />
             <InputComponent 
                 onAddItem={handleAddItem} 
@@ -74,6 +75,7 @@ const App = () => {
             />
             <button onClick={handleSubmit}>Get Recipes</button>
             {isSubmitted && <RecipeListComponent llamaRecipes={llamaRecipes} isLoading={isLoading} />}
+        </span>
         </div>
     );
 }
